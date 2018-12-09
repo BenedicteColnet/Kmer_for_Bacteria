0726a6b44655298f17ff78213fba2ff45f27f9bb
Benedicte Colnet <benedicte.colnet@gmail.com>
2018-12-09 20:25:28 +0100

    Code plus modulaire

--- kmer.py
+++ kmer.py
@@ -8,24 +8,44 @@ Created on Wed Dec  5 17:18:49 2018
 from itertools import *
 import numpy as np
 import matplotlib.pyplot as plt
-
 import time
 
-query='GCF_002973605.1_ASM297360v1_genomic.fna'
-
-
-def k_mer(fileName,k):
-    start = time.time()
-    words=list(product('ATGC', repeat = k))
+def read_genome(fileName):
+    
+    """
+    ENTRY adress of fasta file with genome (str)
+    
+    FUNCTION read the file and extract genome
+    
+    EXIT query only (str)
+    
+    """
     with open(fileName,'r') as genomeFile:
         lines = genomeFile.readlines()
-    genom = ''
+    genom_sequence = ''
     for line in lines:
         if not '>' in line: 
-            genom += line
-    genom = genom.replace('\n','')
+            genom_sequence += line
+    genom_sequence = genom_sequence.replace('\n','')
+    
+    return genom_sequence
+
+
+
+
+def k_mer_naive(genom,k):
+            
+    """
+    ENTRY genom sequence (str) and size of kmer asked (int)
+    
+    FUNCTION read the genom and count the number of kmer
+    
+    EXIT dictionnary ({kmer:number})
+    
+    """    
     genomLength = len(genom)
     result = {}
+    words=list_of_possible_kmer('ATGC',k)
     for wo in words:
         w = ''
         for i in range(k):
@@ -33,12 +53,31 @@ def k_mer(fileName,k):
         result[w] = 0
     for i in range(genomLength-k+1):
         result[genom[i:i+k]]+=1
-    end = time.time()
-    return (result, end-start)
-    
-        
-result = k_mer(query,10)
 
-#figure(num=None, figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')
-#plt.bar(list(result[0].keys()), result[0].values(), color='g')
-#plt.show()
+    return result
+
+
+
+def list_of_possible_kmer(letters,k):
+    
+    """
+    ENTRY letters possible for e.g 'ATGC' (str) and size of kmer (int)
+    
+    FUNCTION create a list of all possible words of size k
+    
+    EXIT list of possible words of size k (list)
+    
+    """  
+    words=list(product(letters, repeat = k))
+    return words
+
+
+
+query='GCF_002973605.1_ASM297360v1_genomic.fna'    
+start = time.time()       
+result = k_mer_naive(read_genome(query),3)
+end = time.time()
+
+plt.figure()
+plt.bar(result.keys(), result.values(), color='g')
+plt.plot()
