1312bc5fe771fff5efe673e51612b0d2f6ea6f6b
Yasser <yasser.mohseni@yahoo.com>
2019-01-05 15:47:37 +0100

    SVM and StratificationKFOLD are added

--- classification.py
+++ classification.py
@@ -6,23 +6,25 @@ Classification base on kmer vectors previously build
 
 import pandas as pd
 import numpy as np
+"""
 import keras
 from keras.layers import Dense
 from keras.models import Sequential
 from keras.utils import to_categorical
-import sklearn
+"""
 from sklearn.neural_network import MLPClassifier
+from sklearn import svm
 from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_validate
-from itertools import *
-
+from sklearn.model_selection import cross_val_score
 from sklearn.decomposition import PCA, IncrementalPCA, KernelPCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-
-import matplotlib.pyplot as plt
 from sklearn import cluster
 from sklearn.cluster import KMeans
+from itertools import *
+import matplotlib.pyplot as plt
+import time
+
 
 """
 OBJECT CONSIDERED
@@ -35,11 +37,8 @@ def list_of_possible_kmer(letters,k):
     
     """
     ENTRY letters possible for e.g 'ATGC' (str) and size of kmer (int)
-    
     FUNCTION create a list of all possible words of size k
-    
     OUT list of possible words of size k (list)
-    
     """  
     words=list(product(letters, repeat = k))
     return words
@@ -105,6 +104,67 @@ def PreProcessTuples(name_of_npy):
         output.append(prof)
     return output
 
+def CrossValidation(model,X,Y,cross_val=10):
+    startT = time.time()
+    cvScores = cross_val_score(model, X, Y, cv=cross_val, scoring='accuracy')*100
+    endT = time.time()
+    return cvScores,endT-startT
+
+
+def CrossValidationStratification(model,X,Y,cross_val=10):
+    startT = time.time()
+    skf = StratifiedKFold(n_splits=cross_val, random_state=7, shuffle=True)
+    cvScores = []
+    for train_index, test_index in skf.split(X,Y):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = Y[train_index], Y[test_index]
+        model.fit(X_train,y_train)
+        cvScores.append(model.score(X_test,y_test))
+    endT = time.time()
+    cvScores = np.array(cvScores)
+    return cvScores,endT-startT
+
+
+def AnalyzeNNMLP(X,y):
+    modelMLP = MLPClassifier(solver='lbfgs', alpha=1e-7,hidden_layer_sizes=(10, 5))
+    #scoresMLP, totalDurMLP = CrossValidation(modelMLP,X,y,10)
+    scoresMLP, totalDurMLP = CrossValidationStratification(modelMLP,X,y,10)
+    print("MLP CV, Accuracy: %0.2f (+/- %0.2f), total time: %f" 
+          % (scoresMLP.mean(), scoresMLP.std() * 2, totalDurMLP))
+
+def AnalyzeNLSVM(X,y):
+    for kern in ['linear', 'poly', 'rbf']:
+        for cValue in [10e-9,0.1,10]:
+            modelSVM = svm.SVC(gamma = 'auto', C=cValue, kernel=kern)
+            #scoresSVM, totalDurSVM = CrossValidation(modelSVM,X,y,10)
+            scoresSVM, totalDurSVM = CrossValidationStratification(modelSVM,X,y,10)
+            print("SVM CV, Kernel: " + kern + ", C value: " + str(cValue) + " Accuracy: %0.2f (+/- %0.2f), total time: %f" 
+                  % (scoresSVM.mean(), scoresSVM.std() * 2, totalDurSVM))
+
+def PreProcessKmerData(name_of_npy, list_of_k):
+    #mapping=np.load("mapping.npy")
+    tuples=PreProcessTuples(name_of_npy)
+    df=Tuples2DF(tuples, list_of_k)
+    df=df.sample(frac=1) #For suffle
+    #df_numerized=df.replace({'Name': mapping})
+    #Y=df_numerized["Name"]
+    #X=df_numerized.drop(['Type', 'Name'], axis=1)
+    Y = pd.factorize(df["Name"])[0]
+    X=df.drop(['Type', 'Name'], axis=1)    
+    return X, Y
+
+
+X, y = PreProcessKmerData("train_profiles_10.npy", [3,4])
+AnalyzeNNMLP(X.values,y)
+AnalyzeNLSVM(X.values,y)
+
+
+
+
+
+
+
+########################################################################################################################
 """
 def neural_network_from_dataframe(X, y):
     model = MLPClassifier(solver='lbfgs', alpha=1e-5,hidden_layer_sizes=(10, 5)).fit(X,y)
@@ -115,38 +175,12 @@ def only_testing_neural_network(X_test,Y_test,model):
      Score = model.score(X_test,Y_test)
      return Score
 """
- 
-def CrossValidation(X,Y,cross_val=10):
-    cvscores = sklearn.model_selection.cross_val_score(model, X, Y, cv=cross_val, scoring='roc_auc')*100
-    return cvscores
 
-def PreProcessKmerData(name_of_npy, list_of_k):
-    #mapping=np.load("mapping.npy")
-    tuples=PreProcessTuples(name_of_npy)
-    df=Tuples2DF(tuples, list_of_k)
-    df=df.sample(frac=1)
-    #df_numerized=df.replace({'Name': mapping})
-    #Y=df_numerized["Name"]
-    #X=df_numerized.drop(['Type', 'Name'], axis=1)
-    Y = pd.factorize(df["Name"])
-    X=df.drop(['Type', 'Name'], axis=1)    
-    return X, Y
-
-
-X, y = PreProcessKmerData("train_profiles_10.npy", [3,4])
-modelMLP = MLPClassifier(solver='lbfgs', alpha=1e-7,hidden_layer_sizes=(10, 5))
-scores = CrossValidation(modelMLP,X,y,10)
-print(scores)
-#model=neural_network_from_dataframe(X, y)
-#score = only_testing_neural_network(X_test,y_test,model)
-
-
-
-
-########################################################################################################################
 """
 Test area
 """
+#model=neural_network_from_dataframe(X, y)
+#score = only_testing_neural_network(X_test,y_test,model)
 
 """
 tuples_test=[('bact','a',[0.5,0,0.5,0]),('bact','b',[0,0.5,0,0.5]), ('bact','c',[0,0,0,1]), ('bact','d',[0,0.2,0,0.8]),
@@ -161,3 +195,9 @@ tuples_test=[('bact','a',[0.5,0,0.5,0]),('bact','b',[0,0.5,0,0.5]), ('bact','c',
 
 
 #X, y= PreProcessKmer("profiles.npy", [1])
+
+
+##Stratification:
+"""
+
+"""
